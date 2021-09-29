923a21e35f19ec0212383420f9e312412b03859c
Henry Trinh <62784409+HenryTrin@users.noreply.github.com>
2021-09-29 09:50:34 -0500

    Add files via upload

---
+++ NLP2/Chatterbot.py
@@ -0,0 +1,48 @@
+import random
+import re
+import sys
+import collections
+
+"""
+Things to do
+
+2 inputs
+arg 0 = 1,2,3 (unigram, bi,tri)
+
+arg 1- infinite is the input text files
+
+so that means we want to have the files be read
+
+the input files are txt files
+
+We take each text file and lowercase and split them 
+Also take each puntuation and seprate them.
+
+regex for first punctation = ^[.!?]+?
+so now we need the regex for seperating puncaion
+
+steps
+chatterbot 1 input1.txt input2.txt
+first take the input files
+lower + split + seperate them
+*remember this can be any input files greater then 1
+so while len sys[argv]
+go to each input files
+
+
+put them into keys(tokens) and value(counter for each)
+have it count the number as it goes in the input files
+
+unigram calc
+probabilty 
+so takes single words
+then uses probabiltiy to make the sentences
+then print
+
+
+"""
+def main(argv):
+   return 0
+
+if __name__ == "__main__":
+   main()
