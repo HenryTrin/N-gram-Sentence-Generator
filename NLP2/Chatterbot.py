1d0f734ffa14c37a3a331ea4c4bd48760cd1ad93
Henry Trinh <62784409+HenryTrin@users.noreply.github.com>
2021-10-03 20:40:42 -0500

    Update Chatterbot.py

--- NLP2/Chatterbot.py
+++ NLP2/Chatterbot.py
@@ -39,23 +39,72 @@ so takes single words
 then uses probabiltiy to make the sentences
 then print
 
-
+links used :
+https://realpython.com/python-counter/ 
+https://www.pythontutorial.net/python-basics/python-read-text-file/ 
+https://www.geeksforgeeks.org/python-arrays/ 
 """
 
+
 def textFileProccess():
-   return 0
+    allText = ""
+
+	#Needed a way to keep going past any amount of input text files got this
+	#from https://www.geeksforgeeks.org/python-arrays/
+    inputFiles = sys.argv[3:]
+
+	#Needed a way to open files with the number of input files
+	#got this from https://www.geeksforgeeks.org/python-arrays/
+    for file in inputFiles:
+        with open(inputFiles, "r") as file:
+            allText = allText + file.read()
+
+	#Seperate from the inputFiles the words and punctuation
+	
+    return 0
+
+
+def unigramCalculator():
+    print("Unigram")
+    return 0
+
+
+def bigramCalculator():
+    return 0
+
+
+def trigramCalculator():
+    return 0
+
 
 def unigramSentenceGenerator():
-	print("Unigram")
-	return 0
+    print("Unigram")
+    return 0
+
+
 def bigramSentenceGenerator():
-   return 0
+    return 0
+
 
 def trigramSentenceGenerator():
-   return 0
+    return 0
+
+
+"""
+main()
+
+This function handles the command line inputs, argv[1] = unigram, bigram, trigram,
+argv[2] = number of sentences, argv[3-whatever numbers of text files greator then 1] = input text files
+the functions will use to make sentences. Overall just the starter function for the processing
+of random sentence generation. 
+
+:param 
+:return 0
+"""
+
 
 def main(argv):
-	"""
+    """
 	Chatterbot 1 10 pg2554.txt pg2600.txt pg1399.txt
 
 	argv[1] = uni, bi, tri
@@ -67,22 +116,22 @@ def main(argv):
 	word, punctuation = tokens
 
 	each word is key
-	each occurance is a counter
+	each occurance is a tokens
 
-	
+
 	"""
 
+    numberOfSentences = sys.argv[2]
 
-	numberOfSentences = sys.argv[2]
+    if int(sys.argv[1]) == 1:
+        unigramSentenceGenerator(numberOfSentences)
+    elif int(sys.argv[1]) == 2:
+        bigramSentenceGenerator(numberOfSentences)
+    elif int(sys.argv[1]) == 3:
+        trigramSentenceGenerator(numberOfSentences)
+    else:
+        print("Invalid Argument, please use 1-3 and not" + sys.argv[1])
+    return 0
 
-	if int(sys.argv[1]) == 1:
-		unigramSentenceGenerator(numberOfSentences)
-	elif int(sys.argv[1]) == 2:
-		bigramSentenceGenerator(numberOfSentences)
-	elif int(sys.argv[1]) == 3:
-		trigramSentenceGenerator(numberOfSentences)
-	else:
-		print("Invalid Argument, please use 1-3 and not" + sys.argv[1])
-	return 0
 
 main(sys.argv)
